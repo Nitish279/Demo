ec717e310a6d4881901fa0a301efc81222af21af
Nitish Anand <nitish4u029@gmail.com>
2025-05-20 15:25:58 +0530

    removed unwanted codes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,5 @@
-// src/App.js
-import React, { useState } from "react";
-import {
-  defaultTheme,
-  Provider,
-  Heading,
-  View,
-  Divider,
-  RadioGroup,
-  Radio,
-} from "@adobe/react-spectrum";
-import { customTheme } from "./customThemes";
-import "./spectrum-custom.css";
+import "./App.css";
 
-function App() {
-  const themes = [
-    { id: 1, theme: defaultTheme, name: "Default" },
-    { id: 2, theme: customTheme, name: "Custom" },
-  ];
-  const [theme, setTheme] = useState(1);
-  const actualTheme = themes.find((l) => l.id === theme).theme;
-
-  return (
-    <Provider theme={actualTheme} height="100%">
-      <View padding="size-200">
-        <RadioGroup label="Theme" value={theme} onChange={setTheme}>
-          {themes.map((theme) => (
-            <Radio key={theme.id} value={theme.id}>
-              {theme.name}
-            </Radio>
-          ))}
-        </RadioGroup>
-        <Divider />
-        <Heading level={1}>This is a h1 heading</Heading>
-        <View
-          UNSAFE_className="spectrum-custom"
-          padding="size-200"
-          backgroundColor="blue-400"
-          color="red-400"
-        >
-          This is some size-200 text, in a blue-400 container
-        </View>
-      </View>
-    </Provider>
-  );
-}
+const App = () => <div className="app">App</div>;
 
 export default App;
--- src/components/SpectrumButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button } from "@react-spectrum/button";
-
-const SpectrumButton = ({ onClick, children }) => {
-  return <Button onPress={onClick}>{children}</Button>;
-};
-
-export default SpectrumButton;
